f006374b701773f29328dda939ad3ffc8c5738f7
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-22 15:14:10 -0800

    feat: displayed ChatsContacts using ChatCards

--- client/itsapp/src/pages/Chat.js
+++ client/itsapp/src/pages/Chat.js
@@ -1,5 +1,15 @@
+import { ChatsStyles } from "../components/chats/Chats.styled";
+import { Stack } from "react-bootstrap";
+import ChatsContact from "../components/chats/ChatsContact";
+
 const Chat = () => {
-  return <></>;
+  return (
+    <ChatsStyles>
+      <Stack direction="horizontal">
+        <ChatsContact />
+      </Stack>
+    </ChatsStyles>
+  );
 };
 
 export default Chat;

428794bb91604fe60985e1c57b6e7c92fd2c264c
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-22 15:12:20 -0800

    feat: a wrapper to make sure there's nothing on the edges

--- client/itsapp/src/components/Wrapper.styled.js
+++ client/itsapp/src/components/Wrapper.styled.js
@@ -5,4 +5,12 @@ export const Wrapper = styled.div`
   margin-top: 5vh;
   border-radius: 20px;
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.4);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const InnerWrapper = styled.div`
+  width: 90%;
+  height: 90%;
 `;

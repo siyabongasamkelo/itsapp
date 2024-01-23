7d5d13633ba414696e22395118759a9368272d96
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-23 09:16:10 -0800

    feat: added chatbox for writing & displaying messages

--- client/itsapp/src/pages/Chat.js
+++ client/itsapp/src/pages/Chat.js
@@ -1,12 +1,14 @@
 import { ChatsStyles } from "../components/chats/Chats.styled";
 import { Stack } from "react-bootstrap";
 import ChatsContact from "../components/chats/ChatsContact";
+import ChatBox from "../components/chats/ChatBox";
 
 const Chat = () => {
   return (
     <ChatsStyles>
       <Stack direction="horizontal">
         <ChatsContact />
+        <ChatBox />
       </Stack>
     </ChatsStyles>
   );

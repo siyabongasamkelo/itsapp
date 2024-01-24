65282e1ade979feb0f17737fdb7915ce06f25f1c
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-23 16:02:48 -0800

    chore: folder for models

---
+++ server/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    usename: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    image: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 200,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;

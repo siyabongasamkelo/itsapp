7c28b69a1a6b44a64c1df387e6727531b8209398
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-24 12:59:09 -0800

    feat: created registerUser function & onChangeEvents to collect userRegisterInfo

--- client/itsapp/src/components/register/Register.js
+++ client/itsapp/src/components/register/Register.js
@@ -1,6 +1,10 @@
-import { Button } from "react-bootstrap";
+import { Button, Alert } from "react-bootstrap";
 import styled from "styled-components";
 import { TextBox } from "../TextBox";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { baseUrl } from "../../utils/Services";
 
 export const LoginStyles = styled.div`
   height: 80%;
@@ -24,6 +28,32 @@ export const TextBoxs = styled(TextBox)`
 `;
 
 const Register = () => {
+  const [userRegisterInfo, setUserRegisterInfo] = useState({
+    username: "",
+    email: "",
+    password: "",
+    image: null,
+  });
+
+  console.log(userRegisterInfo);
+
+  const registerUser = () => {
+    const formData = new FormData();
+    formData.append("image", userRegisterInfo.image);
+    formData.append("username", userRegisterInfo.username);
+    formData.append("email", userRegisterInfo.email);
+    formData.append("password", userRegisterInfo.password);
+
+    axios
+      .post(`${baseUrl}/user/register`, formData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <LoginStyles>
       <div className=" d-flex justify-content-center ">
@@ -31,25 +61,61 @@ const Register = () => {
           <div>
             <Label>Username : </Label>
 
-            <TextBoxs />
-          </div>
-          <div>
-            <Label>Prifile Picture : </Label>
-
-            <TextBoxs type="file" />
+            <TextBoxs
+              type="text"
+              onChange={(e) => {
+                setUserRegisterInfo({
+                  ...userRegisterInfo,
+                  username: e.target.value,
+                });
+              }}
+            />
           </div>
           <div>
             <Label>Email : </Label>
 
-            <TextBoxs />
+            <TextBoxs
+              type="email"
+              onChange={(e) => {
+                setUserRegisterInfo({
+                  ...userRegisterInfo,
+                  email: e.target.value,
+                });
+              }}
+            />
           </div>
           <div>
             <Label>Password :</Label>
 
-            <TextBoxs />
+            <TextBoxs
+              type="password"
+              onChange={(e) => {
+                setUserRegisterInfo({
+                  ...userRegisterInfo,
+                  password: e.target.value,
+                });
+              }}
+            />
           </div>
           <div>
-            <Button className=" mt-4 " style={{ width: "90%" }}>
+            <Label>Prifile Picture : </Label>
+
+            <TextBoxs
+              type="file"
+              onChange={(e) => {
+                setUserRegisterInfo({
+                  ...userRegisterInfo,
+                  image: e.target.files[0],
+                });
+              }}
+            />
+          </div>
+          <div>
+            <Button
+              className=" mt-4 "
+              style={{ width: "90%" }}
+              onClick={registerUser}
+            >
               Login
             </Button>
           </div>

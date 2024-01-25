c30377eb74c67c276b31efaf25649d837abe40a5
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-25 12:28:33 -0800

    style: cleaning up

--- client/itsapp/src/context/AuthContext.js
+++ client/itsapp/src/context/AuthContext.js
@@ -1,9 +1,14 @@
-import { useState, useEffect, useCallback, createContext } from "react";
-import { baseUrl, postRequest } from "../utils/Services";
+import { useState, useEffect, createContext } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const user = localStorage.getItem("User");
+
+    setUser(user);
+  }, []);
 
   return (
     <AuthContext.Provider

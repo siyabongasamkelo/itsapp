8f03c8bdd1da2627a8824377953a53b06a9fbbf2
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-25 12:30:04 -0800

    feat: created getAllUsers function

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -86,4 +86,14 @@ const findUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser };
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+};
+
+module.exports = { registerUser, loginUser, getAllUsers };

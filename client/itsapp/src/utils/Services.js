446904a3e268350b61f52445c966d1f81c07b981
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-25 08:07:25 -0800

    fix: use async await instead of then()

--- client/itsapp/src/utils/Services.js
+++ client/itsapp/src/utils/Services.js
@@ -2,15 +2,10 @@ import axios from "axios";
 
 export const baseUrl = "http://localhost:5000";
 export const postRequest = async (url, data) => {
-  console.log({ data });
-  axios
-    .post(url, data)
-    .then((res) => {
-      console.log(res);
-      return { data: res, error: false };
-    })
-    .catch((err) => {
-      console.log(err);
-      return { data: err, error: true };
-    });
+  try {
+    const registerUser = await axios.post(url, data);
+    return { data: registerUser, error: false };
+  } catch (error) {
+    return { data: error, error: true };
+  }
 };

32c2e9938d6e5026cb6b3aa9302a3251085ea0f8
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-25 12:32:56 -0800

    context chat related stff

---
+++ client/itsapp/src/context/ChatContext.js
@@ -0,0 +1,50 @@
+import { useState, useEffect, createContext, useCallback } from "react";
+import { io } from "socket.io-client";
+import { baseUrl, postRequest } from "../utils/Services";
+import { getUsers } from "../hooks/useGetUsers";
+
+export const ChatContext = createContext();
+
+export const ChatContextProvider = ({ children, user }) => {
+  const [socket, setSocket] = useState(null);
+  const [chats, setChats] = useState([]);
+
+  console.log(chats);
+
+  //connecting socket io
+  useEffect(() => {
+    const newSocket = io("http://localhost:5000");
+    setSocket(newSocket);
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [user]);
+
+  // getting all chats
+  useEffect(() => {
+    const getAllChats = async () => {
+      const getChats = await getUsers();
+      setChats(getChats);
+    };
+    getAllChats();
+  }, [user]);
+
+  const createChatRoom = useCallback(async (creater, joiner) => {
+    if (!creater || joiner) return;
+    try {
+      const chatRoom = postRequest(`${baseUrl}/chat/create`, {
+        creater,
+        joiner,
+      });
+      return chatRoom;
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <ChatContext.Provider value={{ createChatRoom, chats }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};

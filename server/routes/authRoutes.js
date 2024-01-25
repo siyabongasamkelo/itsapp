a5464f7163b073f99644e4138ec1b04b477a79bb
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-25 12:30:56 -0800

    feat: created getAllUsersRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authControllers");
 
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
+router.get("/", authController.getAllUsers);
 
 module.exports = router;

666eaa629f7681c6c1aa08d9ca44c9ff6c19f6d1
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-25 12:29:17 -0800

    feat: created getRequest

--- client/itsapp/src/utils/Services.js
+++ client/itsapp/src/utils/Services.js
@@ -3,8 +3,17 @@ import axios from "axios";
 export const baseUrl = "http://localhost:5000";
 export const postRequest = async (url, data) => {
   try {
-    const registerUser = await axios.post(url, data);
-    return { data: registerUser, error: false };
+    const sendRequest = await axios.post(url, data);
+    return { data: sendRequest, error: false };
+  } catch (error) {
+    return { data: error, error: true };
+  }
+};
+
+export const getRequest = async (url) => {
+  try {
+    const getRequest = await axios.get(url);
+    return { data: getRequest, error: false };
   } catch (error) {
     return { data: error, error: true };
   }

fea1f42fe05ad564a52858ab18c5aa098588e45a
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-25 16:38:37 -0800

    feat: added socketio conenction & getallchats function

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -18,9 +18,18 @@ const createChatRoom = async (req, res) => {
 };
 
 const getChatRooms = async (req, res) => {
+  const { creater, joiner } = req.body;
+
+  if (!creater || !joiner)
+    return res.status(400).json("creater & joiner are required");
   try {
-    const chatRooms = await chatRoomModel.find();
-    res.status(200).json(chatRooms);
+    const chatRoom = await chatRoomModel
+      .findOne()
+      .where("creater")
+      .in([creater, joiner])
+      .where("joiner")
+      .in([creater, joiner]);
+    res.status(200).json(chatRoom);
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ error });

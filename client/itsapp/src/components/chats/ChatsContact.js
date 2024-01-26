99bd0b60961a220d33a483369eb0ba4b59e6ea8b
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-25 16:36:44 -0800

    fix: displayed chatCard dynamically

--- client/itsapp/src/components/chats/ChatsContact.js
+++ client/itsapp/src/components/chats/ChatsContact.js
@@ -4,12 +4,16 @@ import { TextBox } from "../TextBox";
 import { Chat, PersonAdd } from "react-bootstrap-icons";
 import { ChatCartegory } from "./ChartCartegory.styled";
 import ChatCard from "./ChatCard";
+import { useContext } from "react";
+import { ChatContext } from "../../context/ChatContext";
 
 const ChatsContactStyles = styled.div`
   width: 30%;
+  height: 70vh;
 `;
 
 const ChatsContact = () => {
+  const { chats } = useContext(ChatContext);
   return (
     <ChatsContactStyles>
       <Stack direction="vertical">
@@ -25,12 +29,11 @@ const ChatsContact = () => {
           </ChatCartegory>
         </Stack>
         {/* This is where we display all the Contacts of our users... */}
-        <Stack>
-          <ChatCard />
-          <ChatCard />
-          <ChatCard />
-          <ChatCard />
-          <ChatCard />
+        <Stack direction="vertical">
+          {chats &&
+            chats.map((chat, i) => {
+              return <ChatCard key={i} chat={chat} />;
+            })}
         </Stack>
       </Stack>
     </ChatsContactStyles>

789f1649b3b9a5618ddbf126460f8b8db9610d12
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-26 17:40:21 -0800

    feat: adde chat & message related functions like send & get messages

--- client/itsapp/src/context/ChatContext.js
+++ client/itsapp/src/context/ChatContext.js
@@ -2,14 +2,18 @@ import { useState, useEffect, createContext, useCallback } from "react";
 import { io } from "socket.io-client";
 import { baseUrl, postRequest } from "../utils/Services";
 import { getUsers } from "../hooks/useGetUsers";
+import { getMessages } from "../hooks/useGetMessages";
+import axios from "axios";
 
 export const ChatContext = createContext();
 
 export const ChatContextProvider = ({ children, user }) => {
   const [socket, setSocket] = useState(null);
   const [chats, setChats] = useState([]);
+  const [room, setRoom] = useState(null);
+  const [RoomMessages, setRoomMessages] = useState([]);
 
-  console.log(chats);
+  console.log("messages", RoomMessages);
 
   //connecting socket io
   useEffect(() => {
@@ -29,6 +33,40 @@ export const ChatContextProvider = ({ children, user }) => {
     getAllChats();
   }, [user]);
 
+  //getting live messages from socket
+  useEffect(() => {
+    if (socket === null) return;
+    socket.on("get-live-message", (newMessage) => {
+      setRoomMessages((messages) => [...messages, newMessage]);
+    });
+  }, [socket]);
+
+  //   join chatRoom
+  //   this function will be executed when you click the chatCard components automatically
+  const joinRoom = (room) => {
+    if (socket === null) return;
+    if (!room) return;
+    try {
+      console.log("joining room");
+      socket.emit("join-room", room);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //send live message
+  const sendMessage = async (message) => {
+    if (socket === null) return;
+    if (!message) return;
+    try {
+      await socket.emit("send-message", message);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //creating a chat room
+  //this occurs when user clicks any of the chatcard
   const createChatRoom = useCallback(async (creater, joiner) => {
     if (!creater || joiner) return;
     try {
@@ -42,8 +80,31 @@ export const ChatContextProvider = ({ children, user }) => {
     }
   }, []);
 
+  //returns messages within a chat room
+  const getChatMessages = useCallback(async (chatRoomId) => {
+    try {
+      const messages = await getMessages(chatRoomId);
+      setRoomMessages(messages.data);
+      return RoomMessages;
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
-    <ChatContext.Provider value={{ createChatRoom, chats }}>
+    <ChatContext.Provider
+      value={{
+        createChatRoom,
+        chats,
+        getChatMessages,
+        room,
+        setRoom,
+        RoomMessages,
+        setRoomMessages,
+        sendMessage,
+        joinRoom,
+      }}
+    >
       {children}
     </ChatContext.Provider>
   );

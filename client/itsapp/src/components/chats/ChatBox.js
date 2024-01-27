76df2ff39fe83920aba44c76ed13d695a07aa1dd
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-26 17:34:39 -0800

    feat: added sendMessage TextBox & function , displayed messages dinamically

--- client/itsapp/src/components/chats/ChatBox.js
+++ client/itsapp/src/components/chats/ChatBox.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 import MesageCard from "./MesageCard";
+import { useContext, useState } from "react";
+import { ChatContext } from "../../context/ChatContext";
+import { AuthContext } from "../../context/AuthContext";
+import InputEmoji from "react-input-emoji";
+import { Button, Stack } from "react-bootstrap";
+import { Send } from "react-bootstrap-icons";
+
 export const ChatBoxStyles = styled.div`
   height: 70vh;
   width: 70%;
   /* overflow-y: scroll; */
+`;
+export const MessageCover = styled.div`
+  height: 80%;
+  margin-bottom: 30px;
 `;
 
 const FirstMessageDate = styled.p`
@@ -14,31 +25,60 @@ const FirstMessageDate = styled.p`
 `;
 
 const ChatBox = () => {
+  const { RoomMessages, sendMessage } = useContext(ChatContext);
+  const { user } = useContext(AuthContext);
+
+  const [textMessage, setTextMessage] = useState("");
+  const [currentMessage, setCurrentMessage] = useState({
+    author: user?.data?._id,
+    reciever: RoomMessages.reciever,
+    text: textMessage,
+    chatRoom: RoomMessages.chatRoom,
+  });
+
   return (
     <ChatBoxStyles>
-      <div>
-        <FirstMessageDate>Yesterday</FirstMessageDate>
-      </div>
-      <div>
-        <MesageCard
-          message={
-            "this is a message im using to test how it'll work with thechat...."
-          }
-          isCurrentUser={true}
+      <MessageCover>
+        <div>
+          <FirstMessageDate>Yesterday</FirstMessageDate>
+        </div>
+        <div>
+          <MesageCard
+            message={
+              "this is a message im using to test how it'll work with thechat...."
+            }
+            isCurrentUser={true}
+          />
+          {RoomMessages ? (
+            RoomMessages?.map((message, index) => (
+              <MesageCard
+                key={index}
+                message={message?.text}
+                isCurrentUser={user._id === message.author}
+              />
+            ))
+          ) : (
+            <p>There are no messages on this chat...</p>
+          )}
+        </div>
+      </MessageCover>
+      <Stack direction="horizontal">
+        <InputEmoji
+          value={textMessage}
+          onChange={setTextMessage}
+          cleanOnEnter
+          // onEnter={() => {
+          //   sendMessage(currentMessage);
+          // }}
+          placeholder="Type your message here"
         />
-        <MesageCard
-          message={
-            "this is a message im d f d dfdddfd fdfdfddf dfdfd dfdff fdf dffd fd f  ffdfd     asdasdsa   asdsadsadas   df dfdusing to test how it'll work with thechat...."
-          }
-          isCurrentUser={true}
-        />
-        <MesageCard
-          message={
-            "this is a message im using to test how it'll work with thechat...."
-          }
-          isCurrentUser={false}
-        />
-      </div>
+        <Button
+          className=" rounded-5 "
+          onClick={() => sendMessage(currentMessage)}
+        >
+          <Send style={{ transform: "scale(200%)" }} />
+        </Button>
+      </Stack>
     </ChatBoxStyles>
   );
 };

3f208bc63577122a61a4d5d24c59297dcb1468d3
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-28 18:04:47 -0800

    chore: cleaning up dead code

--- server/socketControllers/JoinRoom.js
+++ server/socketControllers/JoinRoom.js
@@ -4,8 +4,3 @@ module.exports = (io, socket) => {
     console.log(`User with ID: ${socket.id} joined room: ${chatRoom}`);
   });
 };
-
-// socket.on("join-room", async (chatRoom) => {
-//     socket.join(chatRoom);
-//     console.log(`User with ID: ${socket.id} joined room: ${chatRoom}`);
-//   });

ec7450a6acba92d0e6c253f93d6c9fd25bea1eb3
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-28 18:05:18 -0800

    chore: cleaning up dead code

--- server/socketControllers/sendMessage.js
+++ server/socketControllers/sendMessage.js
@@ -1,11 +1,6 @@
 const messages = require("../models/messagesModel");
 
 module.exports = async (io, socket) => {
-  // socket.on("join-room", (chatRoom) => {
-  //   socket.join(chatRoom);
-  //   console.log(`User with ID: ${socket.id} joined room: ${chatRoom}`);
-  // });
-
   socket.on("send-message", async (message) => {
     const newMessage = new messages(message);
     await newMessage.save();

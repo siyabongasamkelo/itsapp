680085f16de8d691f85cc8442e813223a363ecab
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-28 18:00:42 -0800

    fix: moved styles to other file to clean up

--- client/itsapp/src/components/chats/ChatCard.js
+++ client/itsapp/src/components/chats/ChatCard.js
@@ -5,50 +5,13 @@ import { ChatContext } from "../../context/ChatContext";
 import { AuthContext } from "../../context/AuthContext";
 import { baseUrl } from "../../utils/Services";
 import axios from "axios";
-
-const Chatstyles = styled.div`
-  width: 90%;
-  margin-top: 20px;
-  border-radius: 10px;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: 0.5s ease-in-out;
-  cursor: pointer;
-  &:hover {
-    background-color: ${(props) => props.theme.light.primary};
-    h3,
-    p {
-      color: white;
-    }
-  }
-`;
-
-const ChatstylesCover = styled.div`
-  height: 80%;
-  width: 90%;
-`;
-
-const UserImg = styled.div`
-  img {
-    width: 50px;
-    height: 50px;
-    border-radius: 10px;
-  }
-`;
-
-const Username = styled.h3`
-  color: ${(props) => props.theme.light.text};
-  font-size: ${(props) => props.theme.size.medium};
-  transition: 0.5s ease-in-out;
-`;
-
-const MessageDate = styled.p`
-  color: ${(props) => props.theme.light.smallText};
-  font-size: ${(props) => props.theme.size.small};
-  transition: 0.5s ease-in-out;
-`;
+import {
+  Chatstyles,
+  ChatstylesCover,
+  MessageDate,
+  UserImg,
+  Username,
+} from "./ChatCardStyles.styled";
 
 const ShortMessage = styled(MessageDate)``;
 

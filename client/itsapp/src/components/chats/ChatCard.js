b0991da693b54fe0881c64e332ac1ebd701ab4d6
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-29 17:51:18 -0800

    feat: displaying short message on chatCard

--- client/itsapp/src/components/chats/ChatCard.js
+++ client/itsapp/src/components/chats/ChatCard.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Stack } from "react-bootstrap";
 import styled from "styled-components";
 import { ChatContext } from "../../context/ChatContext";
@@ -12,6 +12,7 @@ import {
   UserImg,
   Username,
 } from "./ChatCardStyles.styled";
+import moment from "moment";
 
 const ShortMessage = styled(MessageDate)``;
 
@@ -19,28 +20,47 @@ const ChatCard = ({ chat }) => {
   const { user } = useContext(AuthContext);
   const { setRoomMessages, joinRoom, updateCurrentMessage } =
     useContext(ChatContext);
+  const [latestMessage, setLatestMessage] = useState([]);
+  const [shortMessages, setShortMessages] = useState("");
 
   return (
     <Chatstyles
       onClick={async () => {
-        const chatRoom = await axios.post(`${baseUrl}/chats/get`, {
+        let chatRoom = await axios.post(`${baseUrl}/chats/get`, {
           creater: user?.data?._id,
           joiner: chat?._id,
         });
 
-        joinRoom(chatRoom?.data?._id);
+        // if chatRoom doesn't exist
+        if (!chatRoom?.data)
+          chatRoom = await axios.post(`${baseUrl}/chats/create`, {
+            creater: user?.data?._id,
+            joiner: chat?._id,
+          });
+
+        await joinRoom(chatRoom?.data?._id);
 
         const roomMessages = await axios.get(
           `${baseUrl}/messages/get/${chatRoom?.data?._id}`
         );
+
         setRoomMessages(roomMessages?.data);
 
         updateCurrentMessage({
           author: user?.data?._id,
-          reciever: roomMessages?.data[0].reciever,
+          reciever: chat?._id,
           text: "",
           chatRoom: chatRoom?.data?._id,
         });
+
+        //getting the last message so that i can display it on the ChatCard...
+
+        const lastMessage = roomMessages?.data[roomMessages?.data?.length - 1];
+        setLatestMessage(lastMessage);
+
+        const lasteShortMessages = `${latestMessage?.text?.slice(0, 5)}...  `;
+        setShortMessages(lasteShortMessages);
+        // console.log(shortMessage);
       }}
     >
       <ChatstylesCover>
@@ -55,9 +75,11 @@ const ChatCard = ({ chat }) => {
               className=" justify-content-between "
             >
               <Username>{chat.username}</Username>
-              <MessageDate>Thursday</MessageDate>
+              <MessageDate>
+                {moment(latestMessage?.createdAt).calendar()}
+              </MessageDate>
             </Stack>
-            <ShortMessage>Voice mesasge</ShortMessage>
+            <ShortMessage>{shortMessages}</ShortMessage>
           </Stack>
         </Stack>
       </ChatstylesCover>
